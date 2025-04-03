3153b2556a7f00c84170b3478fdcada2c3e9d168
Ma-a-a-rni <marnifalkvard@gmail.com>
2025-04-03 20:49:21 +0200

    update build

--- buildClangVers.bat
+++ buildClangVers.bat
@@ -10,7 +10,7 @@ set CMAKE_CMD=cmake -G "Ninja" ^
   -DCMAKE_CXX_COMPILER=clang++ ^
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
   -DCMAKE_TOOLCHAIN_FILE="C:/Users/MarniFalkvardJoensen/scoop/apps/vcpkg/current/scripts/buildsystems/vcpkg.cmake" ^
-  -B ./build .
+  -B ./buildNinja .
 
 echo %CMAKE_CMD%
 %CMAKE_CMD%
--- buildSolution.bat
+++ buildSolution.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM Set command
-set CMAKE_CMD=cmake -G "Visual Studio 17 2022" -A x64 -B ./build .
+set CMAKE_CMD=cmake -G "Visual Studio 17 2022" -A x64 -B ./buildVS .
 
 REM Run command
 echo %CMAKE_CMD%
