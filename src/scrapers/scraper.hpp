ff1bc592d8e5657742931ab2421c9b5902adeb90
Ma-a-a-rni <marnifalkvard@gmail.com>
2025-04-06 23:08:56 +0200

    Added overload to function for meklarin

--- src/scrapers/scraper.hpp
+++ src/scrapers/scraper.hpp
@@ -1,10 +1,12 @@
 // scraper.hpp
 #pragma once
 
+#include <scrapers/house_model.hpp>
 #include <string>
 
 namespace HT {
 // Downloads the HTML content of the given URL
 std::string downloadHtml(const std::string &url);
 std::string downloadAndSaveHtml(const std::string &url);
+std::string downloadAndSaveHtml(const std::string &url, PropertyType propType);
 } // namespace HT
