3c0cf51e98534d0f7d56b95dbeae7e3d3068b044
Ma-a-a-rni <marnifalkvard@gmail.com>
2025-04-18 23:35:06 +0100

    added skyn property model

---
+++ src/scrapers/skyn/skynModel.hpp
@@ -0,0 +1,20 @@
+// skynModel.hpp
+#pragma once
+#include <string>
+
+struct SkynProperty {
+  std::string id;
+  std::string website;
+  std::string type;
+  std::string ogn_headline;
+  std::string ogn_address;
+  std::string prop_size;
+  std::string prop_ground_size;
+  std::string prop_bedrooms;
+  std::string prop_floors;
+  std::string prop_buildYear;
+  std::string prop_listPrice;
+  std::string prop_latestOffer;
+  std::string prop_validToDate;
+  std::string img;
+};
