96680f47b0154e93a120c578ddb88d4ac7692fdb
Ma-a-a-rni <marnifalkvard@gmail.com>
2025-04-24 11:56:59 +0200

    more props and clang versin build changed

--- buildClangVers.bat
+++ buildClangVers.bat
@@ -1,18 +1,16 @@
 @echo off
+REM ── Bring MSVC & Windows-SDK paths into %PATH%
+call "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat"
 
-REM Set Clang compilers
-set CC=clang
-set CXX=clang++
+set CC=clang-cl
+set CXX=clang-cl
 
-REM Set command
-set CMAKE_CMD=cmake -G "Ninja" ^
-  -DCMAKE_C_COMPILER=clang ^
-  -DCMAKE_CXX_COMPILER=clang++ ^
+cmake -G Ninja ^
+  -DCMAKE_C_COMPILER=%CC% ^
+  -DCMAKE_CXX_COMPILER=%CXX% ^
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
   -DCMAKE_TOOLCHAIN_FILE="C:/Users/MarniFalkvardJoensen/scoop/apps/vcpkg/current/scripts/buildsystems/vcpkg.cmake" ^
-  -B ./build .
+  -B build -S .
 
-echo %CMAKE_CMD%
-%CMAKE_CMD%
-
-pause
+ninja -C build         
+REM or: cmake --build build
