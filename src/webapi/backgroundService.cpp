b184319f44c5bf4bf029c7196327f83b7120ea31
Ma-a-a-rni <marnifalkvard@gmail.com>
2025-04-25 20:09:52 +0200

    better logging for bg service

--- src/webapi/backgroundService.cpp
+++ src/webapi/backgroundService.cpp
@@ -29,6 +29,11 @@ void logTime(const std::string &prefix) {
             << std::put_time(&localTm, "%Y-%m-%d %H:%M:%S") << std::endl;
 }
 
+void logTime(const std::string &prefix, const std::tm &timeinfo) {
+  std::cout << "[" << prefix << "] "
+            << std::put_time(&timeinfo, "%Y-%m-%d %H:%M:%S") << std::endl;
+}
+
 // Helper: Get next scheduled time (20:00, Mon–Fri)
 std::chrono::steady_clock::duration getTimeUntilNextRun() {
   using namespace std::chrono;
@@ -51,17 +56,17 @@ std::chrono::steady_clock::duration getTimeUntilNextRun() {
     nowTm = safeLocaltime(tempT);
   }
 
+  std::time_t nextRunT = system_clock::to_time_t(nextRunTime);
+  std::tm nextRunTm = safeLocaltime(nextRunT);
+
+  logTime("Scheduler");
+  logTime("Scheduler - next run at", nextRunTm);
+
   return duration_cast<steady_clock::duration>(nextRunTime - now);
 }
 
 void scheduleDailyScraper(trantor::EventLoop *loop) {
   auto delay = getTimeUntilNextRun();
-
-  auto secondsUntilNext =
-      std::chrono::duration_cast<std::chrono::seconds>(delay).count();
-  logTime("Scheduler");
-  std::cout << "Scheduled to run in " << secondsUntilNext << " seconds."
-            << std::endl;
 
   loop->runAfter(delay.count() / 1.0e9, [loop]() {
     logTime("Scraper Running");
