b43c17267374c29c061818e7e831866c2d79215f
Marni <marni@falkvard.com>
2025-06-09 14:07:25 +0200

    Updated the batch file build process for windows

--- buildSolution.bat
+++ buildSolution.bat
@@ -1,10 +1,17 @@
 @echo off
+REM ——————————————————————————————
+REM First install the packages in vcpkg.json
+REM vcpkg install
+REM 1) Where vcpkg.exe actually lives on Windows:
+set VCPKG_ROOT=C:/Users/marni/scoop/apps/vcpkg/current
 
-REM Set command
-set CMAKE_CMD=cmake -G "Visual Studio 17 2022" -A x64 -B ./buildVS .
-
-REM Run command
-echo %CMAKE_CMD%
-%CMAKE_CMD%
+cmake ^
+  -G "Visual Studio 17 2022" ^
+  -A x64 ^
+  -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake ^
+  -DVCPKG_TARGET_TRIPLET=x64-windows ^
+  -DVCPKG_MANIFEST_MODE=ON ^
+  -B .\buildVS ^
+  .
 
 pause
