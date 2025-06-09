da0709274979684c9eccbb2c602b09f89e713a4a
Marni <marni@falkvard.com>
2025-06-09 17:10:46 +0200

    Added a arg to run scraper manually

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,12 @@
 #include <scrapers/include/PropertyManager.hpp>
 #include <webapi/webapi.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc > 1 && std::string(argv[1]) == "--scrape") {
+    HT::PropertyManager::runPropertyParsers(false);
+    return 0;
+  }
+
   HT::runServer();
   return 0;
 }
